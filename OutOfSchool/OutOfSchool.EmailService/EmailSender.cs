0c67021260ae6416bc5163f89cb5c5301b218dbf
Elizabeth129 <45245513+Elizabeth129@users.noreply.github.com>
2021-04-12 11:46:10 +0300

    Send email async

--- OutOfSchool/OutOfSchool.EmailService/EmailSender.cs
+++ OutOfSchool/OutOfSchool.EmailService/EmailSender.cs
@@ -1,9 +1,11 @@
 ﻿using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 using System;
 using System.Collections.Generic;
-
+using System.Security.Authentication;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OutOfSchool.EmailService
 {
@@ -20,6 +22,13 @@ namespace OutOfSchool.EmailService
             var emailMessage = CreateEmailMessage(message);
 
             Send(emailMessage);
+        }
+
+        public async Task SendEmailAsync(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+
+            await SendAsync(emailMessage);
         }
 
         private MimeMessage CreateEmailMessage(Message message)
@@ -39,7 +48,9 @@ namespace OutOfSchool.EmailService
             {
                 try
                 {
-                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.CheckCertificateRevocation = false;
+                    
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, SecureSocketOptions.SslOnConnect);
                     client.AuthenticationMechanisms.Remove("XOAUTH2");
                     client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
 
@@ -52,6 +63,32 @@ namespace OutOfSchool.EmailService
                 finally
                 {
                     client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.CheckCertificateRevocation = false;
+
+                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, SecureSocketOptions.StartTls);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
                     client.Dispose();
                 }
             }
